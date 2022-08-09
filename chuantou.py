479e174127576fa69cb27b1453bcd73391ee67c9
艰苦极度 <105758941+jiankujidu@users.noreply.github.com>
2022-08-09 15:52:36 +0800

    Add files via upload

--- chuantou.py
+++ chuantou.py
@@ -1,54 +1,32 @@
 # -*- coding: UTF-8 -*-
-# Version: v2.3
-# Created by lstcml on 2022/04/01
-# 建议定时10分钟：*/10 * * * *
+# Version: v1.1
+# Created by lstcml on 2022/08/08
+# 建议定时60分钟：*/60 * * * *
 
 '''
-V2.3
-1、移除钉钉服务器
-
-V2.2
-1、替换推送逻辑
-
-V2.1
-1、新增自定义更新脚本，默认关闭，启用：新建变量qlnwctupdate，值：true
-2、替换更新接口和服务端获取接口
-
-V2.0
-1、 修复运行中出现切换服务器的问题
-
-v1.9更新记录：
-1、新增http_auth认证，使用：新建变量qlhttpauth，值：账号:密码，例：qinglong:123456
-2、新增在线获取服务器，便于服务器更新
-3、新增服务器自动切换，当穿透失败时自动切换到其他服务器
-4、新增pushplus+推送，需提前在配置文件中配置token
+更新记录：
+v1.1
+1、自动安装必要模块；
+2、支持自定义域名前缀；
 '''
-
-'''
-cron: */10 * * * *
-new Env('内网穿透');
-'''
-
 import os
 import re
 import sys
-import json
 import requests
-import random
 from time import sleep
-from requests.auth import HTTPBasicAuth
 
 def update():
     print("当前运行的脚本版本：" + str(version))
+    sys.stdout.flush()
     try:
-        r1 = requests.get("https://ghproxy.com/https://github.com/jiankujidu/chuantou/raw/main/2.2/nwct.py").text
+        r1 = requests.get("https://raw.githubusercontent.com/jiankujidu/chuantou/main/chuantou.py").text
         r2 = re.findall(re.compile("version = \d.\d"), r1)[0].split("=")[1].strip()
         if float(r2) > version:
-            print("公众号:一起瞎折腾\nQQ交流群:641307462\n发现新版本：" + r2)
-            print("公众号:一起瞎折腾\nQQ交流群:641307462\n正在自动更新脚本...")
-            os.system("kill -9 `ps -ef | grep 'ngrok.py' | grep -v 'grep' | awk '{print $1}'`")
-            os.system("rm -f ngrok.py")
-            os.system("ql raw https://ghproxy.com/https://github.com/jiankujidu/chuantou/raw/main/2.2/nwct.py &")
+            print("发现新版本：" + r2)
+            print("正在自动更新脚本...")
+            sys.stdout.flush()
+            os.system("ql raw https://raw.githubusercontent.com/jiankujidu/chuantou/main/chuantou.py &")
+            os._exit()
     except:
         pass
 
@@ -62,23 +40,13 @@ def other_character(str):
             return True
         else:
             return False
-
-# 下载Ngrok主程序
-def download_ngrok():
-    if not os.path.exists("ngrok.py"):
-        res = requests.get("https://ghproxy.com/https://github.com/jiankujidu/chuantou/raw/main/2.2/ngrok.py")
-        with open("ngrok.py", "wb") as f:
-            f.write(res.content)
-    start_nwct()
-
+            
 # 进程守护
-def process_daemon(qlurl):
+def process_daemon():
+    global qlurl
+    qlurl = get_url()
     try:
-        if qlhttp_auth == "None":
-            res = requests.get(qlurl + "/login").text
-        else:
-            http_auth = qlhttp_auth.split(":")
-            res = requests.get(qlurl + "/login", auth=HTTPBasicAuth(http_auth[0], http_auth[1])).text
+        res = requests.get(qlurl + "/login").text
         if "/images/g5.ico" in res:
             return True
         else:
@@ -86,43 +54,44 @@ def process_daemon(qlurl):
     except:
         return False
 
-# 执行程序
-def start_nwct():
-    global qlhttp_auth
-    servers = get_server()
-    count = len(servers)
-    if qlhttp_auth == "":
-        qlhttp_auth = "None"
-    for i in range(count):
-        qlurl = "http://%s.%s" % (subdomain, servers[i]["subdomain"])
-        if not process_daemon(qlurl):
-            os.system("kill -9 `ps -ef | grep 'ngrok.py' | grep -v 'grep' | awk '{print $1}'`")
-            sleep(2)
-            os.system("python3 ngrok.py %s %s %s %s &" %(servers[i]["server"], servers[i]["port"], subdomain, qlhttp_auth))
-            print("启动中...")
-            sleep(5)
-            if process_daemon(qlurl):
-                if load_send():
-                     print("公众号:一起瞎折腾\nQQ交流群:641307462\n启动成功！\n青龙面板：%s" % qlurl)
-                     send("公众号:一起瞎折腾\nQQ交流群:641307462\n内网穿透通知", "青龙面板访问地址：" + qlurl)
-                break
-            else:
-                if i == count-1:
-                    print("公众号:一起瞎折腾\nQQ交流群:641307462\n启动失败！请重试...")
-                else:
-                    print("公众号:一起瞎折腾\nQQ交流群:641307462\n启动失败！正在切换服务器%s..." % str(i+2))
-        else:
-            print("公众号:一起瞎折腾\nQQ交流群:641307462\n程序运行中...\n青龙面板：%s" % qlurl)
-            break
-
-# 获取服务器地址
-def get_server():
+# 获取穿透url
+def get_url():
     try:
-        res = requests.get("https://ghproxy.com/https://github.com/jiankujidu/chuantou/raw/main/2.2/server.fd").text
-        return json.loads(res)
+        with open('localtunnel.lstcml', encoding='utf-8') as f:
+            _content = f.read()
+            if 'your url is' in _content:
+                print("获取穿透链接成功...")
+                sys.stdout.flush()
+                return _content.split(': ')[1].replace('\n','')
+            else:
+                return 'https://raw.githubusercontent.com/jiankujidu/chuantou'
     except:
-        return json.loads('[{"server":"durl.ga","port":"4443","subdomain":"durl.ga"}]')
+        return 'https://raw.githubusercontent.com/jiankujidu/chuantou'
 
+# 执行程序
+def start_nwct(parameter):
+    if not process_daemon():
+        if parameter == 1:
+            os.system('lt --port 5700 > localtunnel.lstcml &')
+        else:
+            os.system('lt --port 5700 -s ' + subdomain + ' > chuantou.lstcml &')
+        print("正在启动内网穿透...")
+        sleep(10)
+        print("正在检测穿透状态...")
+        sys.stdout.flush()
+        if process_daemon():
+            if load_send():
+                print("公众号:一起瞎折腾\n启动内网穿透成功！\n青龙面板：%s" % qlurl)
+                print("若访问穿透地址出现安全检测界面，点击蓝色'Click to Continue'按钮可跳过！")
+                sys.stdout.flush()
+                send("内网穿透通知", "青龙面板访问地址：" + qlurl)
+        else:
+            print("启动内网穿透失败...")
+            sys.stdout.flush()
+    else:
+        print("穿透程序已在运行...\n青龙面板：%s" % qlurl)
+        sys.stdout.flush()
+        
 # 推送
 def load_send():
     global send
@@ -130,7 +99,7 @@ def load_send():
     sys.path.append(cur_path)
     sendNotifPath = cur_path + "/sendNotify.py"
     if not os.path.exists(sendNotifPath):
-        res = requests.get("https://ghproxy.com/https://github.com/jiankujidu/chuantou/raw/main/2.2/sendNotify.py")
+        res = requests.get("https://raw.githubusercontent.com/jiankujidu/chuantou/main/sendNotify.py")
         with open(sendNotifPath, "wb") as f:
             f.write(res.content)
         
@@ -142,37 +111,34 @@ def load_send():
         return False
 
 if __name__ == '__main__':
-    get_server()
-    version = 2.2
+    version = 1.1
     try:
         subdomain = os.environ['qlsubdomain']
     except:
         subdomain = ""
     try:
-        qlhttp_auth = os.environ['qlhttpauth']
-    except:
-        qlhttp_auth = ""
-    try:
-        token = os.environ['PUSH_PLUS_TOKEN']
-    except:
-        token = ""
-    try:
         check_update = os.environ['qlnwctupdate']
     except:
-        check_update = ""
-
-    if check_update == "true":
+        check_update = "true"
+    try:
+        import requests
+    except:
+        os.system('pip3 install requests >/dev/null 2>&1')
+ 
+    if check_update != "false":
         update()
     else:
-        print("公众号:一起瞎折腾\nQQ交流群:641307462\n变量qlnwctupdate未设置，脚本自动更新未开启！")
-
+        print("变量qlnwctupdate未设置，脚本自动更新未开启！")
+        sys.stdout.flush()
+    if os.system('lt --help >/dev/null 2>&1') !=0:
+        os.system('npm install -g localtunnel >/dev/null 2>&1')
     if len(subdomain) < 1:
-        print("公众号:一起瞎折腾\nQQ交流群:641307462\n请新增变量qlsubdomain指定域名前缀！")
+        print("变量qlsubdomain未设置，将随机分配域名前缀！")
+        sys.stdout.flush()
+        start_nwct(1)
     else:
         if other_character(subdomain):
-            if qlhttp_auth == "" or ":" in qlhttp_auth:
-                download_ngrok()
-            else:
-                print("公众号:一起瞎折腾\nQQ交流群:641307462\n变量qlhttp_auth格式错误！例：qinglong:123456")
+            start_nwct(2)
         else:
-            print("公众号:一起瞎折腾\nQQ交流群:641307462\n变量qlsubdomain仅支持英文数字组合！")
+            print("变量qlsubdomain仅支持英文数字组合！")
+    
